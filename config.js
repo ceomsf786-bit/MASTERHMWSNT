7af632d40f11d9429395aa4104592b629ece3aed
ceomsf786-bit <ceomsf786@gmail.com>
2026-07-08 20:11:58 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// SNT Homework App Supabase config
-// Safe for public GitHub front-end use ONLY when Supabase RLS policies are enabled.
-// IMPORTANT: Do NOT add Supabase secret/service_role keys here.
-
-window.SNT_SUPABASE_URL = 'https://ejnkopeekfiedzjgyyiv.supabase.co';
-window.SNT_SUPABASE_ANON_KEY = 'sb_publishable_8XmkfD9AtuFCx8G0UFPuVQ_QncfvPgl';
